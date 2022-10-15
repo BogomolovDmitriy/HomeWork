2d2f93cbee716142c68a2e7d8ef80da8076113e9
Dmitriy <dimgod80@yandex.ru>
2022-10-16 00:23:47 +0800

    add first cod

--- FirstEx/Program.cs
+++ FirstEx/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 10);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+if (num1 > num2)
+{
+    Console.Write("Максимальное часло: ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("Максимальное часло: ");
+    Console.WriteLine(num2);
+}
