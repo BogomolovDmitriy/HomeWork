e255a3e754be08bae3f53a15ebb9ccf412ee48de
Dmitriy <dimgod80@yandex.ru>
2022-10-16 01:05:20 +0800

    add fourth cod

--- FourthEx/Program.cs
+++ FourthEx/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int num = new Random().Next(1, 10);
+Console.Write("Выбрано число: ");
+Console.WriteLine(num);
+int count = 1;
+
+while (count <= num)
+{
+    if (num == 1)
+    {
+        Console.WriteLine("До этого числа нет четных чисел.");
+    }
+    
+    if ((count % 2) == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
